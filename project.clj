1f7efa7425acc6d5a670ead6cc2eb78bd591c5aa
J Irving <j@lollyshouse.ca>
2018-03-09 14:37:52 -0500

    Version 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ca.orchard-labs/proprio "1.0.1-SNAPSHOT"
+(defproject ca.orchard-labs/proprio "1.0.1"
   :description "A library for interacting with AWS Kinesis"
   :url "http://github.com/orchard-labs/proprio"
   :license {:name "Eclipse Public License"
